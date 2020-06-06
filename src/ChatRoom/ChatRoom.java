c9486510648dc16acbc64d548b898e389078d96d
Myjacklee <2251222797@qq.com>
2020-06-06 23:04:44 +0800

    优化了用户界面，修复了一些bug
    1. 优化了服务器界面，用户界面的设计
    2. 添加了用户列表显示后台逻辑代码
    3. 修复了一些bug：连接服务器后客户端的用户列表不能显示自己的名字，服务器群转发消息把消息发给发送消息的用户
    4. 添加了消息框时间显示功能

--- src/ChatRoom/ChatRoom.java
+++ src/ChatRoom/ChatRoom.java
@@ -8,9 +8,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
@@ -25,6 +25,8 @@ import java.io.PrintWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -38,9 +40,7 @@ public class ChatRoom extends JFrame {
 	private JButton button_close_room;
 	private JButton button_send_message;
 	private JPanel contentPane;
-	protected JTextField textField_IP;
 	protected JTextField textField_PORT;
-	protected JTextField textField_room_name;
 	protected JTextArea textPane_online_users;
 	protected JTextArea textPane_message;
 	protected JTextArea textArea_send_message;
@@ -52,6 +52,7 @@ public class ChatRoom extends JFrame {
 	private ServerSocket serverSocket;
 	private ServerThread serverThread;
 	private DefaultListModel listModel;
+	private JList userList;
 	/**
 	 * Launch the application.
 	 */
@@ -74,39 +75,32 @@ public class ChatRoom extends JFrame {
 	public ChatRoom() {
 		setTitle("聊天室 服务器端");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 695, 635);
+		setBounds(100, 100, 695, 560);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		listModel = new DefaultListModel();
+		userList=new JList(listModel);
+		userList.setLocation(27, 0);
 		
 		JLabel lblIp = new JLabel("参数配置");
 		lblIp.setFont(new Font("黑体", Font.PLAIN, 17));
-		lblIp.setBounds(23, 18, 70, 33);
+		lblIp.setBounds(27, 13, 70, 33);
 		contentPane.add(lblIp);
-		
-		JLabel lblIp_1 = new JLabel("IP：");
-		lblIp_1.setVerticalAlignment(SwingConstants.BOTTOM);
-		lblIp_1.setFont(new Font("黑体", Font.PLAIN, 17));
-		lblIp_1.setBounds(23, 96, 46, 20);
-		contentPane.add(lblIp_1);
-		
-		textField_IP = new JTextField();
-		textField_IP.setBounds(106, 94, 140, 24);
-		contentPane.add(textField_IP);
-		textField_IP.setColumns(10);
 		
 		JLabel lblPort = new JLabel("PORT：");
 		lblPort.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblPort.setFont(new Font("黑体", Font.PLAIN, 17));
-		lblPort.setBounds(23, 133, 57, 20);
+		lblPort.setBounds(27, 53, 57, 20);
 		contentPane.add(lblPort);
 		
 		textField_PORT = new JTextField();
+		textField_PORT.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		textField_PORT.setText("1024");
 		textField_PORT.setColumns(10);
-		textField_PORT.setBounds(106, 131, 140, 24);
+		textField_PORT.setBounds(81, 47, 73, 33);
 		contentPane.add(textField_PORT);
 		
 		this.addWindowListener(new WindowAdapter() {
@@ -119,93 +113,75 @@ public class ChatRoom extends JFrame {
 		});
 		
 		button_create_room = new JButton("创建聊天室");
-		button_create_room.setBounds(23, 190, 110, 35);
+		button_create_room.setBounds(27, 90, 110, 35);
 		contentPane.add(button_create_room);
 		button_create_room.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				if(isConnected){
 					JOptionPane.showMessageDialog(null, "已处于连接状态，不能重复连接！","错误",JOptionPane.ERROR_MESSAGE);
 				}
 				try {
 					Integer port=Integer.parseInt(textField_PORT.getText());
 					serverStart(port);
-					textPane_message.append("服务器启动成功！当前端口号："+port+"\r\n");
+					textPane_message.append(getTime()+"服务器启动成功！当前端口号："+port+"\r\n");
 					JOptionPane.showMessageDialog(null, "服务器启动成功！");
 					textField_PORT.setEnabled(false);
 					button_create_room.setEnabled(false);	
 					button_close_room.setEnabled(true);
 					button_send_message.setEnabled(true);
 				} catch (Exception e2) {
-					// TODO: handle exception
 					JOptionPane.showMessageDialog(null, e2.getMessage(),"错误",JOptionPane.ERROR_MESSAGE);
 				}
-				
 			}
 		});
 		
 		JLabel label = new JLabel("聊天室动态");
 		label.setFont(new Font("黑体", Font.PLAIN, 17));
-		label.setBounds(362, 18, 103, 33);
+		label.setBounds(283, 13, 103, 33);
 		contentPane.add(label);
 		
 		textPane_message = new JTextArea();
 		jsp_message=new JScrollPane(textPane_message);
-		jsp_message.setBounds(361, 55, 290, 275);
+		jsp_message.setBounds(283, 55, 368, 275);
 		contentPane.add(jsp_message);
 		textPane_message.setEditable(false);
 		
 		JLabel label_1 = new JLabel("当前在线用户");
 		label_1.setFont(new Font("黑体", Font.PLAIN, 17));
-		label_1.setBounds(23, 240, 121, 33);
+		label_1.setBounds(23, 138, 121, 33);
 		contentPane.add(label_1);
 		
-		textPane_online_users = new JTextArea();
-		jsp_online_user=new JScrollPane(textPane_online_users);
-		jsp_online_user.setBounds(23, 280, 290, 258);
+		jsp_online_user=new JScrollPane(userList);
+		jsp_online_user.setBounds(23, 178, 242, 320);
 		contentPane.add(jsp_online_user);
-		textPane_online_users.setEditable(false);
-		
-		JLabel label_2 = new JLabel("聊天室名：");
-		label_2.setVerticalAlignment(SwingConstants.BOTTOM);
-		label_2.setFont(new Font("黑体", Font.PLAIN, 17));
-		label_2.setBounds(23, 57, 85, 20);
-		contentPane.add(label_2);
-		
-		textField_room_name = new JTextField();
-		textField_room_name.setColumns(10);
-		textField_room_name.setBounds(106, 55, 139, 24);
-		contentPane.add(textField_room_name);
 		
 		button_close_room = new JButton("关闭聊天室");
-		button_close_room.setBounds(155, 190, 110, 35);
+		button_close_room.setBounds(155, 90, 110, 35);
 		contentPane.add(button_close_room);
 		button_close_room.setEnabled(false);
 		
 	
 		textArea_send_message = new JTextArea();
 		jsp_send_message=new JScrollPane(textArea_send_message);
-		jsp_send_message.setBounds(362, 363, 289, 125);
+		jsp_send_message.setBounds(283, 363, 368, 95);
 		contentPane.add(jsp_send_message);
 		
 		JLabel label_3 = new JLabel("消息编辑框");
 		label_3.setFont(new Font("黑体", Font.PLAIN, 17));
-		label_3.setBounds(362, 329, 103, 33);
+		label_3.setBounds(283, 329, 368, 33);
 		contentPane.add(label_3);
 		
 		button_send_message = new JButton("发送");
-		button_send_message.setBounds(556, 503, 95, 35);
+		button_send_message.setBounds(556, 463, 95, 35);
 		contentPane.add(button_send_message);
 		button_send_message.setEnabled(false);
 		
 		//打击发送按钮出发的服务时间
 		button_send_message.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				if(!isConnected){
 					JOptionPane.showMessageDialog(null, "服务器还未启动，不能够发送消息！","错误",JOptionPane.ERROR_MESSAGE);
 					return;
@@ -216,19 +192,19 @@ public class ChatRoom extends JFrame {
 				}
 				String message=textArea_send_message.getText().trim();
 				if(message==null||"".equals(message)){
-					sendServerMessage(message);
-					textPane_message.append("服务器:"+message+"\r\n");
-					textArea_send_message.setText("");
+					JOptionPane.showMessageDialog(null, "消息不能为空！","错误",JOptionPane.ERROR_MESSAGE);
+
 				}
+				sendServerMessage(message);
+				textPane_message.append(getTime()+"[服务器]:"+message+"\r\n");
+				textArea_send_message.setText("");				
 			}
 		});
-		
 		//单击按钮停止服务器事件
 		button_close_room.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				if(!isConnected){
 					JOptionPane.showMessageDialog(null, "服务器还未启动，无需停止！","错误",JOptionPane.ERROR_MESSAGE);
 					return;
@@ -238,26 +214,27 @@ public class ChatRoom extends JFrame {
 					button_create_room.setEnabled(true);
 					button_close_room.setEnabled(false);
 					button_send_message.setEnabled(false);
+					textField_PORT.setEditable(true);
 					textPane_message.append("服务器成功停止！\r\n");
 					JOptionPane.showMessageDialog(null, "服务器成功停止！");
 				} catch (Exception e3) {
-					// TODO: handle exception
 					JOptionPane.showMessageDialog(null, "停止服务器发生异常！","错误",JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 	}
-
+	private String getTime(){
+		Date date=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		return "["+sdf.format(date)+"]";
+	}
 	protected void sendServerMessage(String message) {
-		// TODO Auto-generated method stub
 		for(int i=clients.size()-1;i>=0;i--){
-			clients.get(i).getWriter().println("服务器(多人消息):"+message);
+			clients.get(i).getWriter().println("[服务器]:"+message);
 			clients.get(i).getWriter().flush();
 		}
 	}
-
 	protected void closeServer() {
-		// TODO Auto-generated method stub
 		try {
 			if(serverThread!=null){
 				serverThread.stop();
@@ -279,14 +256,11 @@ public class ChatRoom extends JFrame {
 			listModel.removeAllElements();// 清空用户列表
 			isConnected=false;
 		} catch (IOException e) {
-			// TODO: handle exception
 			e.printStackTrace();
 			isConnected=true;
 		}
 	}
-
 	protected void serverStart(Integer port) throws IOException {
-		// TODO Auto-generated method stub
 		try {
 			clients=new ArrayList<ClientThread>();	
 			serverSocket=new ServerSocket(port);
@@ -294,11 +268,9 @@ public class ChatRoom extends JFrame {
 			serverThread.start();	
 			isConnected=true;
 		} catch (BindException e) {
-			// TODO: handle exception
 			isConnected=false;
 			throw new BindException("端口号已被占用，请换一个！");
 		}catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 			isConnected=false;
 			throw new BindException("启动服务器异常！");
@@ -319,14 +291,11 @@ public class ChatRoom extends JFrame {
 					clientThread.start();
 					clients.add(clientThread);
 					listModel.addElement(clientThread.getUser().getName());
-					textPane_message.append(clientThread.getUser().getName()+" "+clientThread.getUser().getIp()+" 上线啦！\r\n");
+					textPane_message.append(getTime()+clientThread.getUser().getName()+" "+clientThread.getUser().getIp()+" 上线啦！\r\n");
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-
-			
 		}
 	}
 	class ClientThread extends Thread{
@@ -357,28 +326,25 @@ public class ChatRoom extends JFrame {
 				StringTokenizer stringTokenizer =new StringTokenizer(inf,"@");
 				user=new UserBean(stringTokenizer.nextToken(),stringTokenizer.nextToken());
 				//反馈当前连接成功信息
-				writer.println(user.getName()+" "+user.getIp()+"与服务器连接成功！");
+				writer.println("[服务器]"+user.getName()+" "+user.getIp()+"与服务器连接成功！");
 				writer.flush();
 				//反馈当前在线用户信息
 				if(clients.size()>0){
 					String temp="";
 					for(int i=clients.size()-1;i>=0;i--){
-						temp+=(clients.get(i).getUser().getName()+"/"+clients.get(i).getUser().getIp())+"@";
+						temp+=(clients.get(i).getUser().getName()+"@"+clients.get(i).getUser().getIp())+"@";
 					}
 					writer.println("USERLIST@"+clients.size()+"@"+temp);
 					writer.flush();
 				}
 				//向所有的在线用户发送该用户的上线动态
 				for(int i= clients.size()-1;i>=0;i--){
-					clients.get(i).getWriter().println("ADD@"+user.getName()+" "+user.getIp());
+					clients.get(i).getWriter().println("ADD@"+user.getName()+"@"+user.getIp());
 					clients.get(i).getWriter().flush();
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
 		}
 		@Override
 		public void run(){
@@ -387,7 +353,7 @@ public class ChatRoom extends JFrame {
 				while(true){
 					message=reader.readLine();//接收客户端的消息
 					if("CLOSE".equals(message)){
-						textPane_message.append(this.getUser().getName()+" "+this.getUser().getIp()+" 下线\r\n");
+						textPane_message.append(getTime()+this.getUser().getName()+" "+this.getUser().getIp()+" 下线\r\n");
 						reader.close();
 						writer.close();
 						socket.close();
@@ -409,25 +375,26 @@ public class ChatRoom extends JFrame {
 						//将收到的消息转发出去
 						dispatcherMessage(message);
 					}
-					
 				}
 			} catch (IOException e) {
-				// TODO: handle exception
+				e.printStackTrace();
 			}
-			
 		}
 	}
+	//接收到各个用户的消息后转发消息
 	public void dispatcherMessage(String message) {
-		// TODO Auto-generated method stub
 		StringTokenizer stringTokenizer=new StringTokenizer(message,"@");
 		String sourse=stringTokenizer.nextToken();
 		String owner=stringTokenizer.nextToken();
 		String content=stringTokenizer.nextToken();
 		message="["+sourse+"]:"+content;
-		textPane_message.append(message+"\r\n");
+		textPane_message.append(getTime()+message+"\r\n");
 		if("ALL".equals(owner)){
-			for(int i=clients.size()-1;i>=0;i++){
-				clients.get(i).getWriter().println("(多人消息)"+message);
+			for(int i=clients.size()-1;i>=0;i--){
+				if(clients.get(i).getUser().getName().equals(sourse)){
+					continue;
+				}
+				clients.get(i).getWriter().println(message);
 				clients.get(i).getWriter().flush();
 			}
 		}
--- src/ChatRoom/ChatRoomClient.java
+++ src/ChatRoom/ChatRoomClient.java
@@ -9,37 +9,31 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
-import learn.User;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
-import java.awt.Frame;
-import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 import javax.swing.JTextField;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.ListModel;
 
 public class ChatRoomClient extends JFrame {
 
-	private JTextArea textPane = new JTextArea();
-	private JScrollPane JSP=new JScrollPane(textPane);
 	private JList userList;
 	
 	private JPanel contentPane;
@@ -110,37 +104,39 @@ public class ChatRoomClient extends JFrame {
 		
 		JLabel lblPort = new JLabel("PORT：");
 		lblPort.setFont(new Font("黑体", Font.PLAIN, 17));
-		lblPort.setBounds(31, 72, 53, 31);
+		lblPort.setBounds(31, 83, 53, 31);
 		contentPane.add(lblPort);
 		
 		textField_IP = new JTextField("127.0.0.1");
-		textField_IP.setBounds(93, 44, 128, 24);
+		textField_IP.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		textField_IP.setBounds(84, 44, 175, 33);
 		contentPane.add(textField_IP);
 		textField_IP.setColumns(10);
 		
 		textField_PORT = new JTextField();
+		textField_PORT.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		textField_PORT.setColumns(10);
-		textField_PORT.setBounds(93, 73, 128, 24);
+		textField_PORT.setBounds(84, 83, 175, 33);
 		contentPane.add(textField_PORT);
 		
 		JLabel label = new JLabel("昵称：");
 		label.setFont(new Font("黑体", Font.PLAIN, 17));
-		label.setBounds(31, 99, 53, 31);
+		label.setBounds(31, 123, 53, 31);
 		contentPane.add(label);
 		
 		textField_nickname = new JTextField();
+		textField_nickname.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		textField_nickname.setColumns(10);
-		textField_nickname.setBounds(93, 103, 128, 24);
+		textField_nickname.setBounds(84, 123, 175, 33);
 		contentPane.add(textField_nickname);
 		
 		button_join_logout = new JButton("加入聊天室");
-		button_join_logout.setBounds(31, 143, 107, 27);
+		button_join_logout.setBounds(31, 164, 110, 35);
 		contentPane.add(button_join_logout);
 		button_join_logout.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				int port;
 				String nickname;
 				String ip;
@@ -151,7 +147,6 @@ public class ChatRoomClient extends JFrame {
 					try {
 						port=Integer.parseInt(textField_PORT.getText().trim());
 					} catch (NumberFormatException e2) {
-						// TODO: handle exception
 						throw new Exception("端口号不符合要求！应该为整数！");
 					}
 					ip=textField_IP.getText().trim();
@@ -160,15 +155,18 @@ public class ChatRoomClient extends JFrame {
 						throw new Exception("姓名和服务器不能为空！");
 					}
 					boolean connectFlag=connectServer(port,ip,nickname);
-					if(connectFlag){
+					if(!connectFlag){
 						throw new Exception("与服务器连接失败！");
 					}
-					setTitle("当前用户："+nickname);
+					setTitle(getTitle()+" 当前用户："+nickname);
 					button_send_message.setEnabled(true);
 					button_join_logout.setEnabled(false);
 					button_close.setEnabled(true);
+					textField_IP.setEditable(false);
+					textField_PORT.setEditable(false);
+					textField_nickname.setEditable(false);
+					listModel.addElement(nickname);
 				} catch (Exception e2) {
-					// TODO: handle exception
 					JOptionPane.showMessageDialog(null, e2.getMessage(),"错误",JOptionPane.ERROR_MESSAGE);
 				}
 			}
@@ -176,13 +174,11 @@ public class ChatRoomClient extends JFrame {
 		
 		label_1 = new JLabel("当前在线人员");
 		label_1.setFont(new Font("黑体", Font.PLAIN, 17));
-		label_1.setBounds(31, 196, 128, 31);
+		label_1.setBounds(31, 206, 128, 31);
 		contentPane.add(label_1);
 		
-		textPane_online_users = new JTextArea();
-		textPane_online_users.setEditable(false);
-		jsp_online_users=new JScrollPane(textPane_online_users);
-		jsp_online_users.setBounds(31,228,190,261);
+		jsp_online_users=new JScrollPane(userList);
+		jsp_online_users.setBounds(31,234,228,261);
 		contentPane.add(jsp_online_users);
 		
 		label_2 = new JLabel("聊天室实时动态");
@@ -207,9 +203,27 @@ public class ChatRoomClient extends JFrame {
 		contentPane.add(label_3);
 		
 		button_send_message = new JButton("发送");
-		button_send_message.setBounds(563, 463, 80, 27);
+		button_send_message.setBounds(563, 463, 80, 35);
 		contentPane.add(button_send_message);
 		button_send_message.setEnabled(false);
+		button_send_message.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(!isConnected){
+					JOptionPane.showMessageDialog(null,"还未连接聊天室服务器，不能够发送消息","错误",JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				String message=textPane_send_message.getText().trim();
+				if("".equals(message)){
+					JOptionPane.showMessageDialog(null, "发送的消息不能够为空！","错误",JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				sendMessage(textField_nickname.getText() + "@" + "ALL" + "@" + message);
+				textPane_get_message.append(getTime()+"["+textField_nickname.getText()+"]"+message+"\r\n");
+				textPane_send_message.setText("");
+			}
+		});
 		
 		button_close = new JButton("退出聊天室");
 		button_close.addActionListener(new ActionListener() {
@@ -224,16 +238,20 @@ public class ChatRoomClient extends JFrame {
 						throw new Exception("断开连接发生异常");
 					}
 					JOptionPane.showMessageDialog(null, "成功断开连接");
+					textPane_get_message.append(getTime()+"与服务器:"+textField_IP.getText()+"端口号:"+textField_PORT.getText()+" 成功断开连接");
 					button_close.setEnabled(false);
 					button_join_logout.setEnabled(true);
 					button_send_message.setEnabled(false);
+					textField_IP.setEditable(true);
+					textField_nickname.setEditable(true);
+					textField_PORT.setEditable(true);
+					listModel.removeAllElements();
 				} catch (Exception e3) {
-					// TODO: handle exception
 					JOptionPane.showMessageDialog(null,e3.getMessage(),"错误",JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
-		button_close.setBounds(144, 143, 107, 27);
+		button_close.setBounds(149, 163, 110, 35);
 		button_close.setEnabled(false);
 		contentPane.add(button_close);
 		
@@ -245,13 +263,9 @@ public class ChatRoomClient extends JFrame {
 				System.exit(0);
 			}
 		});
-		
-		
-
 	}
 
 	protected synchronized boolean closeConnection() {
-		// TODO Auto-generated method stub
 		try {
 			sendMessage("CLOSE");
 			messsageThread.stop();
@@ -267,7 +281,6 @@ public class ChatRoomClient extends JFrame {
 			isConnected=false;
 			return true;
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 			isConnected=true;
 			return false;
@@ -275,7 +288,6 @@ public class ChatRoomClient extends JFrame {
 	}
 
 	protected boolean connectServer(int port, String ip, String nickname) {
-		// TODO Auto-generated method stub
 		try {
 			//创建客户端socket
 			socket =new Socket(ip,port);
@@ -287,24 +299,25 @@ public class ChatRoomClient extends JFrame {
 			isConnected=true;
 			return true;
 		} catch (Exception e) {	
-			// TODO: handle exception
-			textPane_get_message.append("与IP："+ip+" 端口号："+port+" 的服务器连接失败\r\n");
+			textPane_get_message.append(getTime()+"与IP："+ip+" 端口号："+port+" 的服务器连接失败\r\n");
 			isConnected=false;
 			return false;
 		}
 	}
 	//发送消息
 	private void sendMessage(String string) {
-		// TODO Auto-generated method stub
 		writer.println(string);
 		writer.flush();
 	}
-
+	private String getTime(){
+		Date date=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		return "["+sdf.format(date)+"]";
+	}
 	class MesssageThread extends Thread{
 		private BufferedReader reader;
 		private JTextArea textArea;
 		public MesssageThread(BufferedReader reader,JTextArea textArea) {
-			// TODO Auto-generated constructor stub
 			this.reader=reader;
 			this.textArea=textArea;
 		}
@@ -325,10 +338,11 @@ public class ChatRoomClient extends JFrame {
 			String message="";
 			while(true){
 				try {
-					StringTokenizer stringTokenizer=new StringTokenizer(message,"/@");
+					message=reader.readLine();
+					StringTokenizer stringTokenizer=new StringTokenizer(message,"@");
 					String command=stringTokenizer.nextToken();
-					if("CLOSE".equals(command)){
-						textPane_get_message.append("服务器已经关闭!\r\n");
+					if("CLOSE".equals(command)){	//服务器关闭聊天室，所有在线用户全部下线
+						textPane_get_message.append(getTime()+"服务器已经关闭!\r\n");
 						closeCon();
 						return;
 					}else if("ADD".equals(command)){	//用户上线更新列表
@@ -344,7 +358,7 @@ public class ChatRoomClient extends JFrame {
 						UserBean user=onlineUsers.get(username);
 						onlineUsers.remove(user);
 						listModel.removeElement(username);
-					}else if("USERLIST".equals(command)){	//加载当前在线的用户列表
+					}else if("USERLIST".equals(command)){	//连接成功之后，服务器发来当前在线的用户列表
 						int size=Integer.parseInt(stringTokenizer.nextToken());
 						String username="";
 						String userIP="";
@@ -356,10 +370,9 @@ public class ChatRoomClient extends JFrame {
 							listModel.addElement(username);
 						}
 					}else{
-						textPane_get_message.append(message+"\r\n");
+						textPane_get_message.append(getTime()+message+"\r\n");
 					}
 				} catch (Exception e) {
-					// TODO: handle exception
 					JOptionPane.showConfirmDialog(null,e.getMessage(),"错误",JOptionPane.ERROR_MESSAGE);
 				}
 			}
